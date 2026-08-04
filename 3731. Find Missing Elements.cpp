45e6cf57fae8cc969165f2a2b29714497d101718
shorafothoshen <shorafothoshen@gmail.com>
2026-08-04 16:12:21 +0600

    3731. Find Missing Elements

--- 3731. Find Missing Elements.cpp
+++ 3731. Find Missing Elements.cpp
@@ -1,38 +1,29 @@
-#include<bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    vector<int> findMissingElements(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
 
-vector<int> missingNum(vector<int>&arr){
-	int n = arr.size() + 1;
-    vector<int>ans;
+        int mx = INT_MIN;
+        int mn = INT_MAX;
 
-    int mx=INT_MIN;
-    int mn=INT_MAX;
-
-    for(int i=0;i<n-1;i++){
-    	mx=max(arr[i],mx);
-    	mn=min(arr[i],mn);
-    }
-
-    int size=mx-mn;
-
-    vector<int> hash(size+1, 0);
-
-    for (int i = 0; i < n - 1; i++) {
-        hash[arr[i]]++;
-    }
-
-    for (int i = 1; i <= n+1; i++) {
-        if (hash[i] == 0) {
-            ans.push_back(i);
+        for (int i = 0; i < n; i++) {
+            mx = max(nums[i], mx);
+            mn = min(nums[i], mn);
         }
-    }
-    return ans;
-}
 
-int main(){
-	vector<int> arr = {5,1};
-    vector<int> res = missingNum(arr);
-    for(auto val:res){
-    	cout<<val<<" ";
+        int size = mx - mn;
+        vector<int> hash(size + 1, 0);
+
+        for (int i = 0; i < nums.size(); i++) {
+            hash[nums[i] - mn]++;
+        }
+
+        for (int i = mn; i <= mx; i++) {
+            if (hash[i - mn] == 0) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
     }
-}
+};

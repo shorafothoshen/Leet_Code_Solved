4e3fc53e067a18c72cead461b8c73a9744db2395
Md. Shorafot Hoshen <shorafothoshen@gmail.com>
2026-08-12 20:32:40 +0600

    2958. Length of Longest Subarray With at Most K Frequency

--- 2958. Length of Longest Subarray With at Most K Frequency.cpp
+++ 2958. Length of Longest Subarray With at Most K Frequency.cpp
@@ -20,3 +20,36 @@ public:
         return result;
     }
 };
+
+
+// Best Approach
+
+class Solution {
+public:
+    int maxSubarrayLength(vector<int>& nums, int k) {
+        int n=nums.size();
+        unordered_map<int,int>mp;
+
+        int i=0,j=0, result=0,cnt=0;
+
+        while(j<n){
+            mp[nums[j]]++;
+            if(mp[nums[j]]==k+1){
+                cnt++;
+            }
+            if(cnt>0){
+                mp[nums[i]]--;
+                if(mp[nums[i]]==k){
+                    cnt--;
+                }
+                i++;
+            }
+            if(cnt==0){
+                result=max(result,j-i+1);
+            }
+            j++;
+        }
+
+        return result;
+    }
+};

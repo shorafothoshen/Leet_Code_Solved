97bde9251bafb19a7ade81e4780af43aa3a517e8
Md. Shorafot Hoshen <shorafothoshen@gmail.com>
2026-08-18 17:00:52 +0600

    3471. Find the Largest Almost Missing Integer

---
+++ 3471. Find the Largest Almost Missing Integer.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count[51] = {};
+
+        for (int i = 0; i <= n - k; i++) {
+            bool seen[51] = {};
+
+            for (int j = i; j < i + k; j++) {
+                if (!seen[nums[j]]) {
+                    count[nums[j]]++;
+                    seen[nums[j]] = true;
+                }
+            }
+        }
+
+        int ans = -1;
+
+        for (int x = 0; x <= 50; x++) {
+            if (count[x] == 1) {
+                ans = x;
+            }
+        }
+
+        return ans;
+    }
+};
